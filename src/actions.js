27b43e172392e0e0c627c8458137757f1dec5533
Karan Krishnani <karan.krishnani@thoughtworks.com>
2024-06-19 16:43:23 -0400

    fixed add order

--- src/actions.js
+++ src/actions.js
@@ -1,21 +1,18 @@
 import { connect } from "node:http2";
 
 export const addOrder = async (args, context) => {
-  const user = context.entities.User.findUnique({
+  const user = await context.entities.User.findUnique({
     where: { id: args.userId },
     include: {
       pharmacy: {
         include: {
-          log: {
-            select: {
-              id: true,
-            },
-          },
+          log: true,
         },
       },
     },
   });
-  return context.entities.Entries.create({
+  console.dir(user);
+  return await context.entities.Entries.create({
     data: {
       ndc: args.ndc,
       quantity: parseInt(args.quantity),
